cde3be7038fd377fec384246cbc341f8ff2dcab5
shenphen123 <44706164+shenphen123@users.noreply.github.com>
2020-05-23 00:24:17 +0530

    added files

---
+++ BasicAssignment.java
@@ -0,0 +1,21 @@
+package com.training.java;
+
+public class BasicAssignment {
+
+	
+	public static void main(String[] args){
+		
+		int i = 55;
+		int j = 40;
+		i = i - j;
+		j = j + i;
+		i = i + j;
+		
+		System.out.println("The value of i and j after swapping:" + " i = " + i + " , j = " + j);
+	
+	
+    } 
+
+	}
+
+
